476c2f164f6a4e6b817cb527eb660f51d506af77
Rashmigautam16 <rashmigaut99@gmail.com>
2023-01-18 12:44:23 +0530

    navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,20 +4,22 @@ import { BsSearch } from "react-icons/bs";
 import { SlUser } from "react-icons/sl";
 import { SlHeart } from "react-icons/sl";
 import { SlHandbag } from "react-icons/sl";
+import apnidukan from "./apnidukan.png"
 import "./navbar.css"
 const Navbar = () => {
   return (
+    <div>
     <Hide below='lg'>
       <Box  boxShadow='sm' >
-        <Flex w={"95%"} align="center" m='auto' h={"75px"} justify="space-around" color={"gray.600"}>
-          <Image w="120px" src='https://images.indianexpress.com/2021/01/myntra.png'  alt='' />
-            <Flex gap={"20px"}>
-              <Heading size={"xs"}>MEN</Heading>
-              <Heading size={"xs"}>WOMEN</Heading>
-              <Heading size={"xs"}>KIDS</Heading>
-              <Heading size={"xs"}>HOME & LIVING</Heading>
-              <Heading size={"xs"}>BEAUTY</Heading>
-              <Heading size={"xs"}>STUDIO</Heading>
+        <Flex w={"95%"} align="center" m='auto' h={"75px"} justify="space-between" color={"gray.600"}>
+          <Image w="120px" src={apnidukan}  alt='' />
+            <Flex  w="35%" justifyContent={"space-between"}>
+              <Heading size={"xs"} id="navmen">MEN</Heading>
+              <Heading size={"xs"} id="navwomen">WOMEN</Heading>
+              <Heading size={"xs"} id="navkid">KIDS</Heading>
+              <Heading size={"xs"} id="navhome">HOME & LIVING</Heading>
+              <Heading size={"xs"} id="navbeauty">BEAUTY</Heading>
+              <Heading size={"xs"} id="navstudio">STUDIO</Heading>
               <i className="fa fa-font-awesome" aria-hidden="true"></i>
             </Flex>
             <Box  id="navsearchbox">
@@ -25,6 +27,37 @@ const Navbar = () => {
                <BsSearch/>
                 <input placeholder='Search for products, brand and more' />
                </Box>
+            </Box>
+            <Flex color={"gray.600"}>
+                <Flex flexDir={"column"} justify="center" align={"center"}>
+                  <SlUser/> 
+                  <Text fontSize={"12px"} fontWeight="bold">Profile</Text>
+
+                </Flex>
+            </Flex>
+            <Flex color={"gray.600"}>
+                <Flex flexDir={"column"} justify="center" align={"center"}>
+                  <SlHeart/> 
+                  <Text fontSize={"12px"} fontWeight="bold">Wishlist</Text>
+                </Flex>
+            </Flex>
+            <Flex color={"gray.600"}>
+                <Flex flexDir={"column"} justify="center" align={"center"}>
+                  <SlHandbag/> 
+                  <Text fontSize={"12px"} fontWeight="bold">Bag</Text>
+                </Flex>
+            </Flex>
+        </Flex>
+      </Box>
+    </Hide>
+    <Show below='lg'>
+      <Box  boxShadow='sm' >
+        <Flex w={"95%"} align="center" m='auto' h={"75px"} justify="space-around" color={"gray.600"}>
+
+          <Image w="120px" src={apnidukan}  alt='apnidukan' />
+           <Flex gap={"30px"}>
+             <Box  >
+             <BsSearch/>
             </Box>
             <Flex color={"gray.600"}>
                 <Flex flexDir={"column"} justify="center" align={"center"}>
@@ -44,9 +77,11 @@ const Navbar = () => {
                   <Text fontSize={"12px"} fontWeight="bold">Bag</Text>
                 </Flex>
             </Flex>
+           </Flex>
         </Flex>
       </Box>
-    </Hide>
+    </Show>
+    </div>
   )
 }
 
---
+++ src/Components/Navbar/dropdwon/dropmen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dropmen = () => {
+  return (
+    <div>dropmen</div>
+  )
+}
+
+export default dropmen
