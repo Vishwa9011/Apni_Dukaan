ec43411922324b9585a0ff62a3788441dff20960
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-01-18 13:04:38 +0530

    navbar mega-menu

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -9,78 +9,100 @@ import "./navbar.css"
 const Navbar = () => {
   return (
     <div>
-    <Hide below='lg'>
-      <Box  boxShadow='sm' >
-        <Flex w={"95%"} align="center" m='auto' h={"75px"} justify="space-between" color={"gray.600"}>
-          <Image w="120px" src={apnidukan}  alt='' />
-            <Flex  w="35%" justifyContent={"space-between"}>
-              <Heading size={"xs"} id="navmen">MEN</Heading>
-              <Heading size={"xs"} id="navwomen">WOMEN</Heading>
-              <Heading size={"xs"} id="navkid">KIDS</Heading>
-              <Heading size={"xs"} id="navhome">HOME & LIVING</Heading>
-              <Heading size={"xs"} id="navbeauty">BEAUTY</Heading>
-              <Heading size={"xs"} id="navstudio">STUDIO</Heading>
+      <Hide below='lg'>
+        <Box boxShadow='sm' >
+          <Flex position={'relative'} w={"95%"} align="center" m='auto' h={"75px"} justify="space-between" color={"gray.600"}>
+            <Image w="120px" src={apnidukan} alt='' />
+            <Flex w="35%" justifyContent={"space-between"}>
+              <Box className='nav-link'>
+                <Heading size={"xs"} id="navmen">MEN</Heading>
+                <Box className='mega-menu'>
+                  <Box className='mega-menu-container'></Box>
+                  sdfasdf
+                </Box>
+              </Box>
+
+
+              <Box>
+                <Heading size={"xs"} id="navwomen">WOMEN</Heading>
+                <Box></Box>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} id="navkid">KIDS</Heading>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} id="navhome">HOME & LIVING</Heading>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} id="navbeauty">BEAUTY</Heading>
+              </Box>
+
+              <Box>
+                <Heading size={"xs"} id="navstudio">STUDIO</Heading>
+              </Box>
               <i className="fa fa-font-awesome" aria-hidden="true"></i>
             </Flex>
-            <Box  id="navsearchbox">
-              <Box id='navsearch'> 
-               <BsSearch/>
+            <Box id="navsearchbox">
+              <Box id='navsearch'>
+                <BsSearch />
                 <input placeholder='Search for products, brand and more' />
-               </Box>
+              </Box>
             </Box>
             <Flex color={"gray.600"}>
-                <Flex flexDir={"column"} justify="center" align={"center"}>
-                  <SlUser/> 
-                  <Text fontSize={"12px"} fontWeight="bold">Profile</Text>
-
-                </Flex>
+              <Flex flexDir={"column"} justify="center" align={"center"}>
+                <SlUser />
+                <Text fontSize={"12px"} fontWeight="bold">Profile</Text>
+              </Flex>
             </Flex>
             <Flex color={"gray.600"}>
+              <Flex flexDir={"column"} justify="center" align={"center"}>
+                <SlHeart />
+                <Text fontSize={"12px"} fontWeight="bold">Wishlist</Text>
+              </Flex>
+            </Flex>
+            <Flex color={"gray.600"}>
+              <Flex flexDir={"column"} justify="center" align={"center"}>
+                <SlHandbag />
+                <Text fontSize={"12px"} fontWeight="bold">Bag</Text>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Box>
+      </Hide>
+      <Show below='lg'>
+        <Box boxShadow='sm' >
+          <Flex w={"95%"} align="center" m='auto' h={"75px"} justify="space-around" color={"gray.600"}>
+
+            <Image w="120px" src={apnidukan} alt='apnidukan' />
+            <Flex gap={"30px"}>
+              <Box  >
+                <BsSearch />
+              </Box>
+              <Flex color={"gray.600"}>
                 <Flex flexDir={"column"} justify="center" align={"center"}>
-                  <SlHeart/> 
+                  <SlUser />
+                  <Text fontSize={"12px"} fontWeight="bold">Profile</Text>
+                </Flex>
+              </Flex>
+              <Flex color={"gray.600"}>
+                <Flex flexDir={"column"} justify="center" align={"center"}>
+                  <SlHeart />
                   <Text fontSize={"12px"} fontWeight="bold">Wishlist</Text>
                 </Flex>
-            </Flex>
-            <Flex color={"gray.600"}>
+              </Flex>
+              <Flex color={"gray.600"}>
                 <Flex flexDir={"column"} justify="center" align={"center"}>
-                  <SlHandbag/> 
+                  <SlHandbag />
                   <Text fontSize={"12px"} fontWeight="bold">Bag</Text>
                 </Flex>
+              </Flex>
             </Flex>
-        </Flex>
-      </Box>
-    </Hide>
-    <Show below='lg'>
-      <Box  boxShadow='sm' >
-        <Flex w={"95%"} align="center" m='auto' h={"75px"} justify="space-around" color={"gray.600"}>
-
-          <Image w="120px" src={apnidukan}  alt='apnidukan' />
-           <Flex gap={"30px"}>
-             <Box  >
-             <BsSearch/>
-            </Box>
-            <Flex color={"gray.600"}>
-                <Flex flexDir={"column"} justify="center" align={"center"}>
-                  <SlUser/> 
-                  <Text fontSize={"12px"} fontWeight="bold">Profile</Text>
-                </Flex>
-            </Flex>
-            <Flex color={"gray.600"}>
-                <Flex flexDir={"column"} justify="center" align={"center"}>
-                  <SlHeart/> 
-                  <Text fontSize={"12px"} fontWeight="bold">Wishlist</Text>
-                </Flex>
-            </Flex>
-            <Flex color={"gray.600"}>
-                <Flex flexDir={"column"} justify="center" align={"center"}>
-                  <SlHandbag/> 
-                  <Text fontSize={"12px"} fontWeight="bold">Bag</Text>
-                </Flex>
-            </Flex>
-           </Flex>
-        </Flex>
-      </Box>
-    </Show>
+          </Flex>
+        </Box>
+      </Show>
     </div>
   )
 }
